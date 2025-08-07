aa6f1b1cffe2c8b7acd9ff95d2bdecc7d3b36301
jonth_miranda <miranda.jonathan@outlook.com>
2025-08-06 23:36:41 -0300

    1.0

---
+++ js/project.js
@@ -0,0 +1,15 @@
+<div class="ProjectList">
+    <button>
+        <span class="title">Tarefa 1</span>
+        <span class="description">Hoje às 14h</span>
+    </button>
+    <button>
+        <span class="title">Tarefa 2</span>
+        <span class="description">Amanhã cedo</span>
+    </button>
+    <button>
+        <span class="title">Tarefa 3</span>
+        <span class="description">Em andamento</span>
+    </button>
+</div>
+<button id="cadProject"><img src="./assets/cad.png" alt="cadProject" class="buttonLogo"></button>
