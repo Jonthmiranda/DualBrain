cd208159bc0394c7a9db1f48f0c36ac4ae39f217
Jonathan Miranda <146142589+Jonthmiranda@users.noreply.github.com>
2025-08-11 16:58:41 -0300

    1.0

--- js/scrum.js
+++ js/scrum.js
@@ -9,3 +9,17 @@
                     <span class="Date">10/08/2025</span>
                 </button>
             </div>
+
+ <div class="AddScrumModal">
+                <h4>Scrum</h4>
+                <textarea type="text" id="Yesterday" name="Yesterday" placeholder="Yesterday"></textarea>
+                <textarea type="text" id="Today" name="Today" placeholder="Today"></textarea>
+                <textarea type="text" id="Locks" name="Locks" placeholder="Locks"></textarea>
+           
+                <button id="AddScrumButton">Add Scrum</button>
+           </div>
+           
+                <div class="IsEmptyModal">
+                    <p>Field is empty</p>
+                    <button id="FillFieldButton">Ok</button>
+                </div>
