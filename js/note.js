09de3fcb65584ead276324e040e7e45aec4747bb
Jonathan Miranda <146142589+Jonthmiranda@users.noreply.github.com>
2025-08-11 16:49:28 -0300

    1.0

--- js/note.js
+++ js/note.js
@@ -4,3 +4,17 @@
     </button>
 </div>
 <button id="cadNote"><img src="./assets/cad.png" alt="cadNote" class="buttonLogo"><span>Add Note</span></button>
+
+    <div class="AddNotesModal">
+                <h4>Add Notes</h4>
+                <textarea type="text" id="Title" name="Title" placeholder="Title"></textarea>
+                <textarea type="text" id="Text" name="Text" placeholder="Note"></textarea>
+           
+                <button id="AddNotesButton">Add Note</button>
+                <button id="CancelButton">Cancel</button>
+           </div>
+           
+                <div class="IsEmptyModal">
+                    <p>Field is empty</p>
+                    <button id="FillFieldButton">Ok</button>
+                </div>
