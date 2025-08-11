1b9c6a4c672f151ae2c0f303679660b33ac3a3dc
jonth_miranda <miranda.jonathan@outlook.com>
2025-08-10 23:39:06 -0300

    1.0

--- js/checklist.js
+++ js/checklist.js
@@ -1,4 +1,17 @@
 <ul class="ChecklistList">
-    <li><input type="checkbox" id="item1"><label for="item1">Criar layout</label></li>
-</ul>
-<button id="cadChecklist"><img src="./assets/cad.png" alt="cadChecklist" class="buttonLogo"><span>Add Checklist</span></button>        
+                <li><input type="checkbox" id="item1"><label for="item1">Criar layout</label></li>
+            </ul>
+            <button id="cadChecklist"><img src="./assets/cad.png" alt="cadChecklist" class="buttonLogo"><span>Add Checklist</span></button>
+
+            <div class="AddChecklistModal">
+                <h4>Add Checklist</h4>
+                <p class="TitleTask">Type the Task:</p>
+                <textarea type="text" id="Task" name="Task"></textarea>
+                
+                <button id="AddChecklistButton">Add Checklist</button>
+                <button id="CancelButton">Cancel</button>
+
+                <div class="IsEmptyModal">
+                    <p>Field is empty</p>
+                    <button id="FillFieldButton">Ok</button>
+                </div>
---
+++ js/scrum.js
@@ -0,0 +1,11 @@
+<div class="ScrumList">
+                <button>
+                    <span class="Date">03/08/2025</span>
+                </button>
+                <button>
+                    <span class="Date">09/09/2025</span>
+                </button>
+                <button>
+                    <span class="Date">10/08/2025</span>
+                </button>
+            </div>
