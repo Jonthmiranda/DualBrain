f5c551f698deb48e952a5a78d14ae224d0653d20
Jonathan Miranda <146142589+Jonthmiranda@users.noreply.github.com>
2025-09-04 13:49:15 -0300

    1.0

--- js/scrum.js
+++ js/scrum.js
@@ -1,24 +1,14 @@
 //IMPORTS
 import { SelectScrums } from './db.js';
 import { InsertScrum } from './db.js';
+import { DeleteProject } from './db.js';
+import { ChecklistScreen } from './checklist.js';
+import { NoteScreen } from './note.js';
+import { ProjectScreen } from './project.js';
 
-//VARIABLES SCRUM LIST
-var ScrumList = document.querySelector(".ScrumList");
-var Date = document.querySelector(".Date");
-
-//VARIABLES ADD SCRUM MODAL
-var AddScrumModal = document.querySelector(".AddScrumModal");
-var Yesterday = document.getElementById("Yesterday");
-var Today = document.getElementById("Today");
-var Locks = document.getElementById("Locks");
-var AddScrumButton = document.getElementById("AddScrumButton");
-
-//VARIABLES IS EMPTY MODAL
-var IsEmptyModal = document.querySelector(".IsEmptyModal");
-var FillFieldButton = document.getElementById("FillFieldButton");
-
-export function ScrumScreen() {
+export function ScrumScreen(ProjectId) {
     document.querySelector("section").innerHTML = `
+    <section>
 <div class="ScrumList">
                 <button>
                     <span class="Date">03/08/2025</span>
@@ -43,5 +33,94 @@ export function ScrumScreen() {
                 <div class="IsEmptyModal">
                     <p>Field is empty</p>
                     <button id="FillFieldButton">Ok</button>
-                </div>`;
+                </div>
+    </section>
+     <nav>
+        <button id="checklist"><img src="./assets/checklist.png" alt="Checklist"class="buttonLogo"><span>Checklist</span></button>
+        <button id="notes"><img src="./assets/notes.png" alt="Notes" class="buttonLogo"><span>Notes</span></button>
+        <button id="scrum"><img src="./assets/scrum.png" alt="Scrum" class="buttonLogo"><span>Scrum</span></button>
+        <button id="menu"><img src="./assets/menu.png" alt="Menu" class="buttonLogo"></button>
+        <div class="MenuContent">
+            <button id="EndProject">Finalize</button>
+            <button id="DeleteProjectBt">Delete</button>
+            <button id="ChangeProject">Change</button>
+            <button id="Help">Help</button>
+        </div>
+    </nav>
+    `;
+    
+    RenderScrum(ProjectId);
+    
+    //VARIABLES SCRUM LIST
+var ScrumList = document.querySelector(".ScrumList");
+var Date = document.querySelector(".Date");
+
+//VARIABLES ADD SCRUM MODAL
+var AddScrumModal = document.querySelector(".AddScrumModal");
+var Yesterday = document.getElementById("Yesterday");
+var Today = document.getElementById("Today");
+var Locks = document.getElementById("Locks");
+var AddScrumButton = document.getElementById("AddScrumButton");
+
+//VARIABLES IS EMPTY MODAL
+var IsEmptyModal = document.querySelector(".IsEmptyModal");
+var FillFieldButton = document.getElementById("FillFieldButton");
+
+ //VARIAVEIS TAGS
+var nav = document.querySelector("nav");
+
+//VARIAVEIS NAV BUTTONS
+var checklist = document.getElementById("checklist");
+var notes = document.getElementById("notes");
+var scrum = document.getElementById("scrum");
+var menu = document.getElementById("menu");
+var EndProject = document.getElementById("EndProject");
+var DeleteProjectBt = document.getElementById("DeleteProjectBt");
+var ChangeProject = document.getElementById("ChangeProject");
+var Help = document.getElementById("Help");
+
+//VARIAVEIS NAV DIVS
+var MenuContent = document.querySelector(".MenuContent");
+
+//MUDANÇAS DE TELAS EVENTS       
+checklist.addEventListener("click", function () {
+    scrum.style.backgroundColor = "#F4F4F4";
+    ChecklistScreen(ProjectId);
+})
+
+notes.addEventListener("click", function () {
+    scrum.style.backgroundColor = "#F4F4F4";
+    NoteScreen(ProjectId);
+})
+
+//change project
+ChangeProject.addEventListener("click", function () {
+    scrum.style.backgroundColor = "#F4F4F4";
+    ProjectScreen();
+})
+
+//delete project
+DeleteProjectBt.addEventListener("click", function () {
+    scrum.style.backgroundColor = "#F4F4F4";
+    DeleteProject(ProjectId);
+    ProjectScreen();
+})
+
+//DIV MENU EVENT
+menu.addEventListener("click", function () {
+
+    if (MenuContent.style.display === "none") {
+        MenuContent.style.display = "block";
+    } else {
+        MenuContent.style.display = "none";
+    }
+})
+
+    async function RenderScrum(ProjectId) {
+        const Scrums = await SelectScrums(ProjectId);
+        const container = document.querySelector(".ScrumList");
+        container.innerHTML = "";
+        scrum.style.backgroundColor = "#d6d4d4";
+        
+    }
 }
